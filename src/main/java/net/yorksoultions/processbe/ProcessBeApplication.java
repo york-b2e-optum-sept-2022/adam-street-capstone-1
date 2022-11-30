17fb5e020b2ecd4d0f5b5fc6dab82b19d86ed85a
adam-street <adam.street.dev@gmail.com>
2022-11-30 13:35:54 -0600

    init

---
+++ src/main/java/net/yorksoultions/processbe/ProcessBeApplication.java
@@ -0,0 +1,13 @@
+package net.yorksoultions.processbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/yorksoultions/processbe/ProcessBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksoultions.processbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
